5cf51ba7f81377d39b4bd2c5f1951b84df0bec96
Iyede Isaiah <a.ajibike@zaposta.com>
2018-07-28 15:34:37 +0100

    third commit

--- tripodApp/tripodAppenv/tripodAppTest/employeeInfo/urls.py
+++ tripodApp/tripodAppenv/tripodAppTest/employeeInfo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('add', views.add_user, name='add_user'),
     path('list', views.list_users, name='list_users'),
 ]
